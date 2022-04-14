41dda4d753492c7b0b44195f787a78f43185ab7a
meyer443 <97495118+meyer443@users.noreply.github.com>
2022-04-13 23:24:04 -0400

    Add files via upload

---
+++ data-quality-checks.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+"""
+This is a script to conduct various data quality checks on data for the final project in ABE 65100
+    created by Justin Meyer
+    last edited 2022-04-13
+"""
+
+import pandas as pd
+import numpy as np
+
+import os
+os.chdir("C:/Users/justi/OneDrive/Documents/GitHub/final-project-meyer443")
+
+def ReadData( fileName ):
+    
+    df = pd.read_csv( fileName, sep=',' ) # read in contents of csv using Pandas
+    df = df.set_index('Reading')
+    # define and initialize the missing data frame
+    colNames = ['Logger','Temperature (C)','Dissolved Oxygen (mg/L)','Depth']
+    ReplacedValuesDF = pd.DataFrame(0, index=["1. No Data","2. Gross Error"], columns=colNames)
+
+    return( df, ReplacedValuesDF )
+
+def Check01_RemoveNoDataValues( DataDF, ReplacedValuesDF ):
+    #This check replaces the defined No Data value with the NumPy NaN value
+    #so that further analysis does not use the No Data values.  Function returns
+    #the modified DataFrame and a count of No Data values replaced.
+    n = len(DataDF)
+    m = n-1
+
+    for i in range (0,m):
+        for j in range(0,3):
+            if DataDF.iloc[i,j] == " ": #removing any values =-999
+                DataDF.iloc[i,j]= np.nan
+       
+    ReplacedValuesDF.iloc[0,0]=DataDF['Logger'].isna().sum()
+    ReplacedValuesDF.iloc[0,1]=DataDF['Temperature (C)'].isna().sum()
+    ReplacedValuesDF.iloc[0,2]=DataDF['Dissolved Oxygen (mg/L)'].isna().sum()
+    ReplacedValuesDF.iloc[0,3]=DataDF['Depth'].isna().sum()
+
+    return( DataDF, ReplacedValuesDF )
+    
+def Check02_GrossErrors( DataDF, ReplacedValuesDF ):
+    #This function checks for gross errors, values well outside the expected 
+    #range, and removes them from the dataset.  The function returns modified 
+    #DataFrames with data the has passed, and counts of data that have not 
+    #passed the check.
+#Loggers
+    for i in range (0,len(DataDF)-1):
+           if (DataDF['Logger'].iloc[i]<99999) or (DataDF['Logger'].iloc[i]>999999): #Logger IDs are 6-digit numbers
+               DataDF['Logger'].iloc[i]= np.nan
+#Temperature
+    for i in range (0,len(DataDF)-1): #replacing values outside of 0 to 35 range with nan
+           if DataDF['Temperature (C)'].iloc[i]< (0) or DataDF['Temperature (C)'].iloc[i] >35:
+               DataDF['Temperature (C)'].iloc[i]= np.nan
+# DO    
+    for i in range (0,len(DataDF)-1): #replacing values outside of 0 to 15 range with nan
+           if DataDF['Dissolved Oxygen (mg/L)'].iloc[i]< (0) or DataDF['Dissolved Oxygen (mg/L)'].iloc[i] >15:
+               DataDF['Dissolved Oxygen (mg/L)'].iloc[i]= np.nan
+#Depth     
+    for i in range (0,len(DataDF)-1): # replacing values outside of 0 to 20 range with nan
+           if (DataDF['Depth'].iloc[i]<0) or (DataDF['Depth'].iloc[i]>20):
+               DataDF['Depth'].iloc[i]= np.nan
+               
+    ReplacedValuesDF.iloc[1,0]=DataDF['Logger'].isna().sum()-ReplacedValuesDF.iloc[0,0]
+    ReplacedValuesDF.iloc[1,1]=DataDF['Temperature (C)'].isna().sum()-ReplacedValuesDF.iloc[0,1]
+    ReplacedValuesDF.iloc[1,2]=DataDF['Dissolved Oxygen (mg/L)'].isna().sum()-ReplacedValuesDF.iloc[0,2]
+    ReplacedValuesDF.iloc[1,3]=DataDF['Depth'].isna().sum()-ReplacedValuesDF.iloc[0,3]
+
+    return( DataDF, ReplacedValuesDF )
+
+# the following condition checks whether we are running as a script, in which 
+# case run the test code, otherwise functions are being imported so do not.
+# put the main routines from your code after this conditional check.
+
+if __name__ == '__main__':
+
+    fileName = "allsites_depth.csv"
+    DataDF, ReplacedValuesDF = ReadData(fileName)
+   
+    print("\nRaw data.....\n", DataDF.describe())
+    
+    DataDF, ReplacedValuesDF = Check01_RemoveNoDataValues( DataDF, ReplacedValuesDF )
+    
+    print("\nMissing values removed.....\n", DataDF.describe())
+    
+    DataDF, ReplacedValuesDF = Check02_GrossErrors( DataDF, ReplacedValuesDF )
+    
+    print("\nCheck for gross errors complete.....\n", DataDF.describe())
+    
+    print("\nAll processing finished.....\n", DataDF.describe())
+    print("\nFinal changed values counts.....\n", ReplacedValuesDF)
+   
+
+#Read Data
+    ReadData('allsites_depth.csv')
+
+#Create copy of raw data
+    Raw = pd.read_csv("allsites_depth.csv", delimiter=",")
+    Raw = Raw.set_index('Reading')
+
+#Plot Data
+    import matplotlib.pyplot as plt
+#Logger 
+    plt.plot(DataDF.index, Raw['Logger'],'b*', label='Raw Data')
+    plt.plot(DataDF.index, DataDF['Logger'],'r*', label='After Data Quality')
+    plt.xlabel('Date')
+    plt.ylabel('Logger ID Number')
+    plt.legend()
+    plt.savefig('Logger.png')
+    plt.close()
+
+#Temperature (C)
+    plt.plot(DataDF.index, Raw['Temperature (C)'],'b*', label='Raw Data')
+    plt.plot(DataDF.index, DataDF['Temperature (C)'],'r*', label='After Data Quality')
+    plt.xlabel('Date')
+    plt.ylabel('Temperature (C)')
+    plt.legend()
+    plt.savefig('Temp.png')
+    plt.close()
+
+#DO
+    plt.plot(DataDF.index, Raw['Dissolved Oxygen (mg/L)'],'b*', label='Raw Data')
+    plt.plot(DataDF.index, DataDF['Dissolved Oxygen (mg/L)'],'r*', label='After Data Quality')
+    plt.xlabel('Date')
+    plt.ylabel('Dissolved Oxygen (mg/L)')
+    plt.legend()
+    plt.savefig('DO.png')
+    plt.close()
+
+#Depth
+    plt.plot(DataDF.index, Raw['Depth'],'b*', label='Raw Data')
+    plt.plot(DataDF.index, DataDF['Depth'],'r*', label='After Data Quality')
+    plt.xlabel('Date')
+    plt.ylabel('Depth (m)')
+    plt.legend()
+    plt.savefig('Depth.png', bbox_inches='tight')
+    plt.close()
+
+
+#Write Data to TAB Deliniated Files
+    DataDF.to_csv('After_DataQuality.txt', sep='\t', index=True)
+
+    ReplacedValuesDF.to_csv('ReplacedValues.txt', sep='\t', index=True)
---
+++ graphical-data-analysis.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+"""
+This is a script to run graphical data analysis on my final project data
+    created by Justin Meyer
+    Last edited 2022-04-13
+"""
+
+# importing modules
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+import seaborn as sns
+import scipy.stats as stats
+
+import os
+os.chdir('C:/Users/justi/OneDrive/Documents/GitHub/final-project-meyer443')
+
+def read_data( fileName ):
+    df = pd.read_csv( fileName, sep=',' ) # read in contents of csv using Pandas
+    
+    df = df.assign(Depth = df['Logger']) # create column for depth with duplicate values from Logger column
+    
+    # replace values in Depth column with recorded depths from Lowrance
+    df.loc[df['Logger'] == 638867, 'Depth'] = 6.5
+    df.loc[df['Logger'] == 280741, 'Depth'] = 11.0
+    df.loc[df['Logger'] == 694855, 'Depth'] = 7.6
+    df.loc[df['Logger'] == 704690, 'Depth'] = 5.9
+    df.loc[df['Logger'] == 666469, 'Depth'] = 3.9
+    df.loc[df['Logger'] == 765378, 'Depth'] = 7.5
+    df.loc[df['Logger'] == 753018, 'Depth'] = 4.5
+    df.loc[df['Logger'] == 691297, 'Depth'] = 5.5
+    df.loc[df['Logger'] == 207032, 'Depth'] = 8.4
+    df.loc[df['Logger'] == 195286, 'Depth'] = 5.8
+    df.loc[df['Logger'] == 679953, 'Depth'] = 4.3
+    df.loc[df['Logger'] == 893976, 'Depth'] = 8.2
+    df.loc[df['Logger'] == 151343, 'Depth'] = 7.9
+    df.loc[df['Logger'] == 569373, 'Depth'] = 5.6
+    df.loc[df['Logger'] == 393808, 'Depth'] = 5.0
+    df.loc[df['Logger'] == 977126, 'Depth'] = 10.7
+    df.loc[df['Logger'] == 270520, 'Depth'] = 10.8
+    df.loc[df['Logger'] == 670871, 'Depth'] = 3.9
+    df.loc[df['Logger'] == 207575, 'Depth'] = 11.0
+    df.loc[df['Logger'] == 244238, 'Depth'] = 8.5
+    df.loc[df['Logger'] == 118921, 'Depth'] = 12.3
+    df.loc[df['Logger'] == 886057, 'Depth'] = 3.4
+    df.loc[df['Logger'] == 704192, 'Depth'] = 3.2
+    df.to_csv(r'C:\Users\justi\OneDrive\Documents\GitHub\final-project-meyer443\allsites_depth.csv', index = None, header = True)
+    print(df)
+    return df # return for use in ensuing functions    
+
+def read_info( fileName ):
+    dataFrame = pd.read_csv( fileName, sep=',') # read in contents of csv using Pandas
+    return dataFrame # return for use in ensuing functions
+       
+def generate_histogram_plot( dataFrame, outFileName ):
+    dataFrame.hist(column='Dissolved Oxygen (mg/L)', bins=20, range=(0,14)) # read oxygen data with 20 bins from 0-14 mg/L (realistic range of concentrations)
+    plt.title('Histogram of Dissolved Oxygen Measurements')
+    plt.xlabel('Dissolved Oxygen (mg/L)')
+    plt.ylabel('Frequency')
+    plt.savefig( outFileName, bbox_inches='tight') # 
+    plt.close() # close pyplot priot to starting next function
+ 
+def generate_kde_plot( dataFrame, outFileName ):
+    sns.kdeplot(dataFrame['Dissolved Oxygen (mg/L)'], bw_adjust=0.7, cut=0) # use DO data to generate kde plot, adjusting bin width and cutting off extreme estimates
+    plt.title('KDE Plot of Dissolved Oxygen Concentrations')
+    plt.xlabel('Dissolved Oxygen (mg/L)')
+    plt.ylabel('Density')
+    plt.savefig( outFileName, bbox_inches='tight' )
+    plt.close()
+
+def generate_lat_long_plot( dataFrame, outFileName ):
+    plt.scatter(dataFrame['longitude'], dataFrame['latitude'], s=10) # plot data loggers using coordinates in scatterplot, 's' to manipulate point size
+    plt.title('Location of data loggers')
+    plt.xlabel('Longitude (degrees)') # x-y axes just like on a map
+    plt.ylabel('Latitude (degrees)')
+    plt.savefig( outFileName, bbox_inches='tight' )
+    plt.close()
+
+def generate_cdf_plot( dataFrame, outFileName ):
+    dpth=np.sort(dataFrame['Dissolved Oxygen (mg/L)']) # sort array of depth data for normalization
+    cd=np.linspace(0,1,len(dataFrame['Dissolved Oxygen (mg/L)'])) # evenly spaced for plotting
+    plt.plot(dpth, cd) # plot depth vs cumulative distribution
+    plt.title('Normalized CDF of Dissolved Oxygen Concentrations')
+    plt.xlabel('Dissolved Oxygen (mg/L)')
+    plt.ylabel('Cumulative Distribution')
+    plt.savefig( outFileName, bbox_inches='tight' )
+    plt.close()
+
+def generate_scatter_plot( dataFrame, outFileName ):
+    plt.scatter(dataFrame['Q'], dataFrame['Dissolved Oxygen (mg/L)'], marker=".") # plot points of depth vs magnitude, "." marker to manipulate point size
+    plt.title('Relationship between Dissolved Oxygen and Internal Data Quality Reading')
+    plt.xlabel('Q')
+    plt.ylabel('Dissolved Oxygen (mg/L)')
+    plt.savefig( outFileName, bbox_inches='tight' )
+    plt.close()
+
+def generate_new_scatter_plot( dataFrame, outFileName ):
+    plt.scatter(dataFrame['Depth'], dataFrame['Dissolved Oxygen (mg/L)'], marker=".") # plot points of depth vs magnitude, "." marker to manipulate point size
+    plt.title('Relationship between Dissolved Oxygen and Depth')
+    plt.xlabel('Depth (m)')
+    plt.ylabel('Dissolved Oxygen (mg/L)')
+    plt.savefig( outFileName, bbox_inches='tight' )
+    plt.close()
+    
+def generate_quantile_plot( dataFrame, outFileName ):
+    stats.probplot(dataFrame['Temperature (C)'], dist='norm', plot=plt)
+    plt.savefig( outFileName, bbox_inches='tight')
+
+# the following condition checks whether we are
+# running as a script, in which case run the test code,
+# or being imported, in which case don't.
+
+if __name__ == '__main__':
+
+    # set input and output file names
+    oxygen = 'allsites.csv'
+    loggerinfo = 'loggerinfo.csv'
+   
+    # open and read data files
+    dataFrame = read_data( oxygen )
+    infoDF = read_info( loggerinfo )
+  
+    # generate histogram figure
+    outHistName = "histogram.png"
+    generate_histogram_plot( dataFrame, outHistName )
+
+    # generate kde figure
+    outKdeName = "kde.png"
+    generate_kde_plot( dataFrame, outKdeName )
+    
+    # generate lat-long figure
+    outLatLongName = "lat-long.png"
+    generate_lat_long_plot( infoDF, outLatLongName )
+
+    # generate cdf figure
+    outCdfName = "cdf.png"
+    generate_cdf_plot( dataFrame, outCdfName )
+    
+        
+    # generate scatter plot figure
+    outScatName = "scatter.png"
+    generate_scatter_plot( dataFrame, outScatName )
+        
+    # generate scatter plot figure
+    outScatName = "depth-scatter.png"
+    generate_new_scatter_plot( dataFrame, outScatName )
+    
+    # generate quantile figure
+    outQuanName = "quantile.png"
+    generate_quantile_plot( dataFrame, outQuanName )
+  
